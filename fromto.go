39256d9a0fc43718b8da195f3fc112de92a7f054
sulubaby <pokefanshere@gmail.com>
2025-07-23 13:32:09 +0300

    Create fromto.go

---
+++ fromto.go
@@ -0,0 +1,34 @@
+package piscine
+
+import "strconv"
+
+func FromTo(from int, to int) string {
+	if from < 0 || from > 99 || to < 0 || to > 99 {
+		return "Invalid\n"
+	}
+	result := ""
+	if from <= to {
+		for i := from; i <= to; i++ {
+			if i < 10 {
+				result += "0" + strconv.Itoa(i)
+			} else {
+				result += strconv.Itoa(i)
+			}
+			if i != to {
+				result += ", "
+			}
+		}
+	} else {
+		for i := from; i >= to; i-- {
+			if i < 10 {
+				result += "0" + strconv.Itoa(i)
+			} else {
+				result += strconv.Itoa(i)
+			}
+			if i != to {
+				result += ", "
+			}
+		}
+	}
+	return result + "\n"
+}
