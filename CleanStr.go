e16b42dbbb40cfb7d0c4d4dc11341a8fae7d9a4b
sulubaby <pokefanshere@gmail.com>
2025-07-23 13:29:58 +0300

    Create CleanStr.go

---
+++ CleanStr.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		z01.PrintRune('\n')
+		return
+	}
+
+	input := os.Args[1]
+	inWord := false
+	printedWord := false
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+		if ch != ' ' && ch != '\t' {
+			if !inWord && printedWord {
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune(rune(ch))
+			inWord = true
+			printedWord = true
+		} else {
+			inWord = false
+		}
+	}
+	z01.PrintRune('\n')
+}
