c5da400b0716b68d084cc3d99ae8ed5cc8f91827
sulubaby <pokefanshere@gmail.com>
2025-07-28 21:11:13 +0300

    Update weareunique.go

--- weareunique.go
+++ weareunique.go
@@ -3,22 +3,23 @@ package piscine
 import "strings"
 
 func WeAreUnique(str1, str2 string) int {
+	counter := 0
+	temp := ""
+
 	if str1 == "" && str2 == "" {
 		return -1
 	}
-
-	uc := make(map[rune]bool)
-
-    for _,char:= range str1{
-        if !strings.Contains(str2 , string(char)) && !uc[char]{
-            uc[char] = true
-        }
-    }
-
-    for _,char:= range str2{
-        if !strings.Contains(str1 , string(char)) && !uc[char]{
-            uc[char] = true
-}
-    }
-    return len(uc)
+	for _, v := range str1 {
+		if !strings.Contains(str2, string(v)) && !strings.Contains(temp, string(v)) {
+			counter++
+			temp += string(v)
+		}
+	}
+	for _, v := range str1 {
+		if !strings.Contains(str1, string(v)) && !strings.Contains(temp, string(v)) {
+			counter++
+			temp += string(v)
+		}
+	}
+	return counter
 }
