4e0bf03c993ccb5a080e4b596e11ca20783e5365
sulubaby <pokefanshere@gmail.com>
2025-08-02 13:34:46 +0300

    Update CleanStr.go

--- CleanStr.go
+++ CleanStr.go
@@ -2,33 +2,30 @@ package main
 
 import (
 	"os"
-
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 2 {
+		z01.PrintRune('\n')
 		return
 	}
-	arr := []string{}
-	temp := ""
 
-	for _, v := range os.Args[1] + " " {
-		if v == ' ' || v == '\t' {
-			if temp != "" {
-				arr = append(arr, temp)
-				temp = ""
-			}
+	str := os.Args[1]
+	inWord := false
+	spaceNeeded := false
+
+	for _, ch := range str {
+		if ch == ' ' || ch == '\t' {
+			inWord = false
 		} else {
-			temp += string(v)
+			if spaceNeeded && !inWord {
+				z01.PrintRune(' ')
+			}
+			z01.PrintRune(ch)
+			inWord = true
+			spaceNeeded = true
 		}
 	}
-	for i, v := range arr {
-		for _, char := range v {
-			z01.PrintRune(char)
-		}
-		if i != len(arr)-1 {
-			z01.PrintRune(' ')
-		}
-	}
+	z01.PrintRune('\n')
 }
